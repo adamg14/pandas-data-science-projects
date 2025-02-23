f1b3caf781fa7e0212cc189ddf7ccb2abe276d28
Adam Worede <adamg2609@gmail.com>
2025-02-23 23:21:11 +0000

    initial commit, pandas data manipulation on netflix film metadata and new york public school student results

---
+++ nyc_school_test_results/index.py
@@ -0,0 +1,25 @@
+# Re-run this cell 
+import pandas as pd
+import numpy as np
+
+# Read in the data
+schools = pd.read_csv("schools.csv")
+
+# Preview the data
+schools.head()
+
+best_math_schools = schools[schools["average_math"] >= 800*0.8][["school_name", "average_math"]].sort_values("average_math", ascending=False)
+
+schools["total_SAT"] = schools["average_math"] + schools["average_reading"] + schools["average_writing"]
+
+top_10_schools = schools.sort_values("total_SAT", ascending=False)[["school_name", "total_SAT"]]
+top_10_schools = top_10_schools.iloc[:10]
+top_10_schools.head()
+
+borough_stats = schools.groupby("borough")["total_SAT"].agg(['count', np.mean, np.std,])
+borough_stats = borough_stats.round(2)
+borough_stats = borough_stats.reset_index()
+borough_stats.columns = "borough", "num_schools", "average_SAT", "std_SAT"
+borough_stats.sort_values("std_SAT", ascending=False)
+largest_std_dev = borough_stats[borough_stats["std_SAT"] == borough_stats["std_SAT"].max()]
+largest_std_dev
